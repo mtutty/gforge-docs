16ef5a76fe80e493a43a25d96738a476011d0b96
Michael Tutty <michael.tutty@gmail.com>
2011-09-19 16:13:53 -0500

    Added stub for download worker

---
+++ GForgeDocWindow/Actions/DownloadProjectWorker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+using GForgeDocWindow.Util;
+using System.ComponentModel;
+
+namespace GForgeDocWindow.Actions {
+    public class DownloadProjectWorker : BackgroundWorker {
+
+    }
+}
